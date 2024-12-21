b801f71c2024e972dabae021a319fb034deb3acf
shadichy <hotrobb360web@gmail.com>
2024-12-21 20:31:26 +0700

    debian build files update
    Signed-off-by: shadichy <hotrobb360web@gmail.com>

--- debian/upstream/metadata.ex
+++
@@ -1,6 +0,0 @@
-# Bug-Database: https://github.com/shadichy/grub-android-x86/issues
-# Bug-Submit: https://github.com/shadichy/grub-android-x86/issues/new
-# Changelog: https://github.com/shadichy/grub-android-x86/blob/master/CHANGES
-# Documentation: https://github.com/shadichy/grub-android-x86/wiki
-# Repository-Browse: https://github.com/shadichy/grub-android-x86
-# Repository: https://github.com/shadichy/grub-android-x86.git
